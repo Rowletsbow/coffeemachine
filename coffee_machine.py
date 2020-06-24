a73313a5705ba6638da456bf65b661ac38fb5e43
Taimur H <66155791+Rowletsbow@users.noreply.github.com>
2020-06-24 18:56:15 +0300

    Update coffee_machine.py

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,85 +1,74 @@
-# THIS PROJECT IS STILL UNDER DEVELOPMENT
-# THE COMMENTED CODE WITHIN '...' CAN BE IGNORED
+from datetime import datetime, time
 
-# ..................................................
-# # takes user input on available amount of ingredients
-# # (of course ideally a sensor that gives an input would
-# # be better)
-# print('How many ml of water are already present in me?')
-# available_water = int(input('ml of water: '))
-# print('How many ml of milk are already present in me?')
-# available_milk = int(input('ml of milk :'))
-# print('How many grams of coffee beans are already present in me?')
-# available_coffee_beans = int(input('grams of coffee beans: '))
-#
-# # takes input on number of cups of coffee user wants
-# print('How many coffee cups should I make?')
-# number_of_cups = int(input('cups: '))
-#
-# # calculates and prints amount of ingredients needed for
-# # number_of_cups entered by user
-# water = 200 * number_of_cups
-# milk = 50 * number_of_cups
-# coffee_beans = 15 * number_of_cups
-# print('\nFor', number_of_cups, 'cups of coffee please add to me the following:\n',
-#       milk, 'ml of milk\n',
-#       water, 'ml of water\n',
-#       coffee_beans, 'g of coffee beans\n')
-#
-# # prints this while calculating next code blocks
-# print('Processing...\n')
-#
-# # compares required ingredients with available ingredients
-# # and prints ability to make or not make requested cups
-# # of coffee (along with extra/maximum cups if possible)
-# if water <= available_water and \
-#         milk <= available_milk and \
-#         coffee_beans <= available_coffee_beans:
-#     # calculates number of extra coffee cups
-#     # machine can make
-#     extra_possible_cups = min((available_water // 200) - number_of_cups,
-#                               (available_milk // 50) - number_of_cups,
-#                               (available_coffee_beans // 15) - number_of_cups)
-#     if extra_possible_cups > 0:
-#         print('Yes, I can make this amount of coffee (and ',
-#               extra_possible_cups,
-#               ' cup(s) more too!\n')
+available_money = 0
+available_water = 0
+available_milk = 0
+available_coffee_beans = 0
+available_disposable_cups = 0
+
+
+def processing():
+    print('Initiating coffee engines')
+    print('Disintegrating coffee beans')
+    print('Boiling water')
+    print('Formulating coffee solution')
+    print('Pouring coffee in the cup')
+    print('Finalising emulsion by adding milk')
+    print('Enjoy! :)')
+    
+
+# def cup_calculator(coffee_ingredients):
+#     cups_possible1 = 0
+#     cups_possible2 = 0
+#     cups_possible3 = 0
+#     cups_possible_list = []
+#     cups_possible_list2 = []
+#     if ingredient_checker(coffee_ingredients):
+#         for key, value in coffee_ingredients.items():
+#             if key == 'water':
+#                 if value != 0:
+#                     cups_possible1 = (available_water // coffee_ingredients['water'])
+#                     cups_possible_list.append(cups_possible1 - number_of_cups)
+#             elif key == 'milk':
+#                 if value != 0:
+#                     cups_possible2 = (available_milk // coffee_ingredients['milk'])
+#                     cups_possible_list.append(cups_possible2 - number_of_cups)
+#             elif key == 'coffee_beans':
+#                 if value != 0:
+#                     cups_possible3 = (available_coffee_beans // coffee_ingredients['coffee_beans'])
+#                     cups_possible_list.append(cups_possible3 - number_of_cups)
+#         extra_possible_cups = max(cups_possible_list)
+#         if extra_possible_cups > 0:
+#             print('Yes, I can make this amount of coffee (and ',
+#                   extra_possible_cups,
+#                   ' cup(s) more too!)\n')
+#         else:
+#             print('Yes, I can make this amount of coffee,'
+#                   ' this is the maximum amount of coffee I can make right now.\n')
 #     else:
-#         print('Yes, I can make this amount of coffee,'
-#               ' this is the maximum amount of coffee I can make right now.\n')
-# else:
-#     # calculates maximum number of coffee cups
-#     # machine can make
-#     maximum_possible_cups = min((available_water // 200),
-#                                 (available_milk // 50),
-#                                 (available_coffee_beans // 15))
-#     print('Sorry, but I can only make ',
-#           maximum_possible_cups,
-#           ' cup(s) right now :(\n')
-#
-# print('Initiating coffee engines')
-# print('Disintegrating coffee beans')
-# print('Boiling water')
-# print('Formulating coffee solution')
-# print('Pouring coffee in the cup')
-# print('Finalising emulsion by adding milk')
-# print('Enjoy! :)')
-# ..................................................
-
-# these variables are re-assigned solely for
-# successful completion of a project and shall be
-# set to default values later
-available_money = 550
-available_water = 400
-available_milk = 540
-available_coffee_beans = 120
-available_disposable_cups = 9
+#         for key, value in coffee_ingredients.items():
+#             if key == 'water':
+#                 if value != 0:
+#                     cups_possible1 = (available_water // coffee_ingredients['water'])
+#                     cups_possible_list2.append(cups_possible1)
+#             elif key == 'milk':
+#                 if value != 0:
+#                     cups_possible2 = (available_milk // coffee_ingredients['milk'])
+#                     cups_possible_list2.append(cups_possible2)
+#             elif key == 'coffee_beans':
+#                 if value != 0:
+#                     cups_possible3 = (available_coffee_beans // coffee_ingredients['coffee_beans'])
+#                     cups_possible_list2.append(cups_possible3)
+#         maximum_possible_cups = min(cups_possible_list2)
+#         print('Sorry, but I can only make ',
+#               maximum_possible_cups,
+#               ' cup(s) right now :(\n')
 
 
 # when called, shows remaining ingredients, cups, and
 # money in the coffee machine
-def show_status():
-    print('Current Status:\n',
+def show_machine_status():
+    print('Current Machine Status:\n',
           available_water, 'ml of water\n',
           available_milk, 'ml of milk\n',
           available_coffee_beans, 'grams of coffee beans\n',
@@ -90,21 +79,33 @@ def show_status():
 # when called, shows list of possible options
 # for the user to choose to make the coffee
 # machine do something
-def show_options():
-    print('What would you like to do?\n',
-          '1. buy\n',
-          '2. fill\n',
-          '3. take\n',
-          '4. remaining\n',
-          '5. exit\n')
+def show_main_menu():
+    now = datetime.now()
+    now_time = now.time()
+    greeting = ''
+    if time(6, 0) <= now_time <= time(12, 0):
+        greeting = 'Good morning!'
+    elif time(12, 0) < now_time < time(18, 0):
+        greeting = 'Good afternoon!'
+    elif time(18, 0) <= now_time <= time(23, 59):
+        greeting = 'Good evening!'
+    else:
+        greeting = 'All the best!'
+
+    print('{} What would you like to do? :)\n'.format(greeting),
+          '1. Buy\n',
+          '2. Fill\n',
+          '3. Take\n',
+          '4. Remaining\n',
+          '5. Exit\n')
 
 
 # changes remaining items in coffee machine
 # according to sign of arguments given e.g.
 # -10 will reduce 10 from variable while 10
 # would increase 10 in the variable
-def change_status(req_water, req_milk, req_coffee_beans,
-                  money, req_cups):
+def change_machine_status(req_water, req_milk, req_coffee_beans,
+                          money, req_cups):
     global available_money
     available_money = available_money + money
     global available_disposable_cups
@@ -117,38 +118,56 @@ def change_status(req_water, req_milk, req_coffee_beans,
     available_milk = available_milk + req_milk
 
 
+class Coffee:
+    def __init__(self,
+                 req_water,
+                 req_milk,
+                 req_coffee_beans,
+                 req_disposable_cups,
+                 cost):
+        self.ingredients = {'water': req_water,
+                            'milk': req_milk,
+                            'coffee_beans': req_coffee_beans,
+                            'disposable_cups': req_disposable_cups}
+        self.cost = cost
+
+
 # compares remaining items in coffee machine with
 # those required by the requested user and prints
-# any ingredient deficit if present along with returning
-# True which is used in other parts of the program
-# THIS FUNCTION IS UNDER DEVELOPMENT
-def compare_status(req_water, req_milk, req_coffee_beans,
-                   req_cups):
+# any ingredient deficit if present
+def ingredient_checker(coffee_ingredients):
     global available_disposable_cups
     global available_water
     global available_coffee_beans
     global available_milk
-    if available_disposable_cups < req_cups:
-        print('Sorry, not enough disposable cups!')
-        return True
-    elif available_water < req_water:
-        print('Sorry, not enough water!')
-        return True
-    elif available_coffee_beans < req_coffee_beans:
-        print('Sorry, not enough coffee beans!')
-        return True
-    elif available_milk < req_milk:
-        print('Sorry, not enough milk!')
-        return True
-    else:
+    no_ingredient_counter = 0
+    for key, value in coffee_ingredients.items():
+        if key == 'water':
+            if value > available_water:
+                print('Sorry, not enough water!')
+                no_ingredient_counter += 1
+        elif key == 'milk':
+            if value > available_milk:
+                print('Sorry, not enough milk!')
+                no_ingredient_counter += 1
+        elif key == 'coffee_beans':
+            if value > available_coffee_beans:
+                print('Sorry, not enough coffee beans!')
+                no_ingredient_counter += 1
+        elif key == 'disposable_cups':
+            if value > available_disposable_cups:
+                print('Sorry, not enough disposable cups!')
+                no_ingredient_counter += 1
+    if no_ingredient_counter > 0:
         return False
+    return True
 
 
 # this is the while loop which contains code for
 # taking input and performing actions of the coffee
 # machine
 while True:
-    show_options()
+    show_main_menu()
     action_selected = input('Enter number corresponding to action: ')
     if action_selected == '1':
         print('Choose type of coffee: \n',
@@ -156,38 +175,62 @@ while True:
               '2. Latte \n',
               '3. Cappuccino\n',
               '4. Back\n')
-        coffee_selected = input('Enter number corresponding to coffee: ')
-        if coffee_selected == '1':
-            if compare_status(250, 0, 16, 1):
+        coffee_menu_selected = input('Enter number corresponding to action: ')
+        if coffee_menu_selected == '1':
+            print('How many coffee cups should I make?')
+            number_of_cups = int(input('cups: '))
+            espresso = Coffee(250, 0, 16, number_of_cups, 4)
+            # cup_calculator(espresso.ingredients)
+            if not ingredient_checker(espresso.ingredients):
                 continue
-            print('I have enough resources, making you a coffee!')
-            change_status(-250, 0, -16, 4, -1)
-        elif coffee_selected == '2':
-            if compare_status(350, 75, 20, 1):
+            processing()
+            change_machine_status(-espresso.ingredients['water'],
+                                  espresso.ingredients['milk'],
+                                  -espresso.ingredients['coffee_beans'], 
+                                  espresso.cost, 
+                                  -number_of_cups)
+        elif coffee_menu_selected == '2':
+            print('How many coffee cups should I make?')
+            number_of_cups = int(input('cups: '))
+            latte = Coffee(350, 75, 20, number_of_cups, 7)
+            # cup_calculator(latte.ingredients)
+            if not ingredient_checker(latte.ingredients):
                 continue
-            print('I have enough resources, making you a coffee!')
-            change_status(-350, -75, -20, 7, -1)
-        elif coffee_selected == '3':
-            if compare_status(200, 100, 12, 1):
+            processing()
+            change_machine_status(-latte.ingredients['water'],
+                                  -latte.ingredients['milk'],
+                                  -latte.ingredients['coffee_beans'], 
+                                  latte.cost, 
+                                  -number_of_cups)
+        elif coffee_menu_selected == '3':
+            print('How many coffee cups should I make?')
+            number_of_cups = int(input('cups: '))
+            cappuccino = Coffee(200, 100, 12, number_of_cups, 6)
+            # cup_calculator(cappuccino.ingredients)
+            if not ingredient_checker(cappuccino.ingredients):
                 continue
-            print('I have enough resources, making you a coffee!')
-            change_status(-200, -100, -12, 6, -1)
-        elif coffee_selected == '4':
+            processing()
+            change_machine_status(-cappuccino.ingredients['water'],
+                                  -cappuccino.ingredients['milk'],
+                                  -cappuccino.ingredients['coffee_beans'],
+                                  cappuccino.cost, 
+                                  -number_of_cups)
+        elif coffee_menu_selected == '4':
             continue
     elif action_selected == '2':
         water_added = int(input('Enter amount of water you want to add in ml: '))
         milk_added = int(input('Enter amount of milk you want to add in ml: '))
         coffee_beans_added = int(input('Enter amount of coffee beans you want to add in grams: '))
         disposable_cups_added = int(input('Enter amount of disposable cups you want to add: '))
-        change_status(water_added,
-                      milk_added,
-                      coffee_beans_added,
-                      0,
-                      disposable_cups_added)
+        change_machine_status(water_added,
+                              milk_added,
+                              coffee_beans_added,
+                              0,
+                              disposable_cups_added)
     elif action_selected == '3':
         print('I will now give you ', available_money, '$')
-        change_status(0, 0, 0, -available_money, 0)
+        change_machine_status(0, 0, 0, -available_money, 0)
     elif action_selected == '4':
-        show_status()
+        show_machine_status()
     elif action_selected == '5':
         break
